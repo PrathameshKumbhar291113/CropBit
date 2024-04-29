4bd24ad94b205a75a5c3bad3ea0ada7d2c88c331
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-04-29 09:36:39 +0530

    [Code Changes]: removed the keys from the Constants.kt

--- app/src/main/java/com/cropbit/utils/Constants.kt
+++ app/src/main/java/com/cropbit/utils/Constants.kt
@@ -6,10 +6,10 @@ object BundleConstants{
 }
 
 object ApiConstants{
-    const val CHAT_BOT_API_KEY = "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"
+    const val CHAT_BOT_API_KEY = ""
     const val CHAT_BOT_BASE_URL = "https://api.openai.com"
     const val CHAT_BOT_END_POINT = "v1/completions"
-    const val WEATHER_FORECAST_API_KEY ="2716639ad315432ca7055306241404"
+    const val WEATHER_FORECAST_API_KEY =""
     const val WEATHER_FORECAST_BASE_URL ="https://api.weatherapi.com"
     const val WEATHER_FORECAST_END_POINT ="v1/current.json"
 }
