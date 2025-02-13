f965c72fa3a62917c7987eff8f9c421481414f99
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2025-02-13 23:57:35 +0530

    [Code Changes]: changed the last location logic to the fetch current location logic.

--- app/src/main/java/com/cropbit/utils/Constants.kt
+++ app/src/main/java/com/cropbit/utils/Constants.kt
@@ -9,8 +9,8 @@ object ApiConstants{
     const val CHAT_BOT_API_KEY = ""
     const val CHAT_BOT_BASE_URL = "https://api.openai.com"
     const val CHAT_BOT_END_POINT = "v1/chat/completions"
-    const val WEATHER_FORECAST_API_KEY =""
     const val WEATHER_FORECAST_BASE_URL ="https://api.weatherapi.com"
     const val WEATHER_FORECAST_END_POINT ="v1/current.json"
     const val GEMINI_API_KEY = ""
+    const val WEATHER_FORECAST_API_KEY =""
 }
